253dbaf07941f3a68aa03c878c4f2212c886789d
Surat Sathi Samanta <surat22517@iiitd.ac.in>
2023-08-30 14:14:04 +0530

    started writing the driver code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        int choiceOne = 0;
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Library Portal Initialised...");
+        while (true) {
+            System.out.println("---------------------------------");
+            System.out.println("1.\tEnter as a librarian");
+            System.out.println("2.\tEnter as a member");
+            System.out.println("3.\tExit");
+            choiceOne = scanner.nextInt();
+            if (choiceOne == 1){
+                System.out.println("You chose 1");
+            }
+            else if (choiceOne == 2){
+                System.out.println("You chose 2");
+            }
+            else if (choiceOne == 3){
+                System.out.println("You chose 3");
+            }
+            else{
+                System.out.println("Incorrect choice, please try again!");
+            }
+        }
     }
 }
